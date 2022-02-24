031ba0e51c0f0442913b14abe8d7cb699d38dc89
amanuel <73557032+ATedy@users.noreply.github.com>
2022-02-24 10:13:09 +0000

    updated

--- src/com/fitness/Consent.java
+++ src/com/fitness/Consent.java
@@ -1,5 +1,24 @@
 package com.fitness;
 
-public class Consent {
+import java.util.Date;
 
+public class Consent {
+  private String signature;
+  private Date date;
+
+  public String getSignature() {
+    return signature;
+  }
+
+  public void setSignature(String signature) {
+    this.signature = signature;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
 }
